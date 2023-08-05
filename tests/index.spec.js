ea74d972879df477b7671e3ede75e353f2b75a60
Sparrow0hawk <alexjcoleman@gmail.com>
2023-08-05 21:25:40 +0100

    add quick title test

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,19 +1,10 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect, } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('index.html')
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+test('has title', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Test page/);
 });
