ed460092ba0be69dceef716af10b691ca3c25db7
Sparrow0hawk <alexjcoleman@gmail.com>
2023-08-05 21:25:35 +0100

    add import path to playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
-
+import path from 'path'
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
